8ab7cd309101a001b3f768b80f3c8ebdea516924
antisad <59409943+antisad@users.noreply.github.com>
2024-05-01 08:03:22 +0300

    Update ytdl.py

--- colab_leecher/downlader/ytdl.py
+++ colab_leecher/downlader/ytdl.py
@@ -96,7 +96,7 @@ def YouTubeDL(url):
             logging.info(d)
 
     ydl_opts = {
-        "format": "bv*",
+        "format": "bestvideo",
         "allow_multiple_video_streams": True,
         "allow_multiple_audio_streams": True,
         "writethumbnail": True,
